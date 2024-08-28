d81e0c64b17edd4e2ae5b6c0a243449e5ba51c14
Artyom Bobkov <artez1997@gmail.com>
2024-08-28 22:05:26 +0300

    Add BookService with not implemented GetByIdAsync method

---
+++ src/Books.Application/Services/BookService.cs
@@ -0,0 +1,12 @@
+﻿using Books.Application.Models;
+
+namespace Books.Application.Services
+{
+    public class BookService : IBookService
+    {
+        public Task<Book?> GetByIdAsync(Guid id, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
