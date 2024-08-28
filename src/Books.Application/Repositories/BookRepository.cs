d8d0c4cf95a8efa7ac93c5a2d32dce8185f66e08
Artyom Bobkov <artez1997@gmail.com>
2024-08-28 22:09:08 +0300

    Add BookRepository

---
+++ src/Books.Application/Repositories/BookRepository.cs
@@ -0,0 +1,12 @@
+﻿using Books.Application.Models;
+
+namespace Books.Application.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        public Task<Book?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Books.Application/Repositories/IBookRepository.cs
@@ -0,0 +1,9 @@
+﻿using Books.Application.Models;
+
+namespace Books.Application.Repositories
+{
+    internal interface IBookRepository
+    {
+        Task<Book?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
