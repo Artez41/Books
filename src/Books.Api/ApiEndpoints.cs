f4bc53875f6ad5d7cd5e458610cfc29f4daf2f0a
Artyom <artez1997@gmail.com>
2024-08-28 20:29:34 +0300

    Create books controller (#2)

---
+++ src/Books.Api/ApiEndpoints.cs
@@ -0,0 +1,12 @@
+﻿namespace Books.Api
+{
+    public static class ApiEndpoints
+    {
+        private const string ApiBase = "api";
+
+        public static class Books
+        {
+            private const string Base = $"{ApiBase}/books";
+        }
+    }
+}
---
+++ src/Books.Api/Controllers/BooksController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Api.Controllers
+{
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
