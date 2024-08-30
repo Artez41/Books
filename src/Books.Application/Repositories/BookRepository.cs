7a3c1d51ce5b6d7bb0c791bee7e59d4fb69fc415
Artyom Bobkov <artez1997@gmail.com>
2024-08-30 21:13:44 +0300

    Add method to get book by id from database

--- src/Books.Application/Repositories/BookRepository.cs
+++ src/Books.Application/Repositories/BookRepository.cs
@@ -1,12 +1,33 @@
-﻿using Books.Application.Models;
+﻿using Dapper;
+using Books.Application.Database;
+using Books.Application.Models;
 
 namespace Books.Application.Repositories
 {
     public class BookRepository : IBookRepository
     {
-        public Task<Book?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+
+        public BookRepository(IDbConnectionFactory dbConnectionFactory)
         {
-            throw new NotImplementedException();
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task<Book?> GetByIdAsync(Guid id, CancellationToken token = default)
+        {
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+
+            var book = await connection.QueryFirstOrDefaultAsync<Book>(
+                new CommandDefinition("""
+                    select *
+                    from books
+                    where id = @id
+                """, new { id }, cancellationToken: token));
+
+            if (book is null)
+                return null;
+
+            return book;
         }
     }
 }
