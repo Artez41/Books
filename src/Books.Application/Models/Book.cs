e5dc55d1aa8228511aef5c06cdf0a29e7a95ee38
Artyom Bobkov <artez1997@gmail.com>
2024-08-31 21:24:59 +0300

    Add regex expression to generate value of the Slug field

--- src/Books.Application/Models/Book.cs
+++ src/Books.Application/Models/Book.cs
@@ -1,14 +1,26 @@
-﻿namespace Books.Application.Models
+﻿using System.Text.RegularExpressions;
+
+namespace Books.Application.Models
 {
-    public class Book
+    public partial class Book
     {
         public required Guid Id { get; init; }
         public required string Title { get; init; }
-        public required string Slug { get; init; }
+        public string Slug => GenerateSlug();
         public required string Author { get; init; }
         public required string Description { get; init; }
         public required int YearOfRelease { get; init; }
         public required int NumberOfPages { get; init; }
         public required List<string> Genres { get; init; } = new();
+
+        private string GenerateSlug()
+        {
+            var sluggedTitle = SlugRegex().Replace(Title, string.Empty)
+                .ToLower().Replace(" ", "-");
+            return $"{sluggedTitle}-{YearOfRelease}";
+        }
+
+        [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, 5)]
+        private static partial Regex SlugRegex();
     }
 }
