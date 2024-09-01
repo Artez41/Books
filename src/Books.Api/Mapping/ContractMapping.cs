0e2d163bf69cf15e8701dbb16e88506112f9907f
Artyom Bobkov <artez1997@gmail.com>
2024-09-01 23:33:13 +0300

    Add ContractMapping class for mapping types

---
+++ src/Books.Api/Mapping/ContractMapping.cs
@@ -0,0 +1,22 @@
+﻿using Books.Application.Models;
+using Books.Contracts.Requests;
+
+namespace Books.Api.Mapping
+{
+    public static class ContractMapping
+    {
+        public static Book MapToBook(this CreateBookRequest request)
+        {
+            return new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                Author = request.Author,
+                Description = request.Description,
+                YearOfRelease = request.YearOfRelease,
+                NumberOfPages = request.NumberOfPages,
+                Genres = request.Genres.ToList()
+            };
+        }
+    }
+}
