bb3a8a44e5ef5b0c617e1c7d1af5d64462e1e0a3
Artyom Bobkov <artez1997@gmail.com>
2024-09-01 23:27:36 +0300

    Add CreateBookRequest contract

---
+++ src/Books.Contracts/Requests/CreateBookRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Books.Contracts.Requests
+{
+    public class CreateBookRequest
+    {
+        public required string Title { get; init; }
+        public required string Author { get; init; }
+        public required string Description { get; init; }
+        public required int YearOfRelease { get; init; }
+        public required int NumberOfPages { get; init; }
+        public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
+    }
+}
