30339ac4091411c142295b9c231055c473652de0
Artyom <artez1997@gmail.com>
2024-09-02 21:04:53 +0300

    Add get book contract (#8)

---
+++ src/Books.Contracts/Responses/BookResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Books.Contracts.Responses
+{
+    public class BookResponse
+    {
+        public required Guid Id { get; init; }
+        public required string Title { get; init; }
+        public required string Author { get; init; }
+        public required string Description { get; init; }
+        public required int NumberOfPages { get; init; }
+        public required int YearOfRelease { get; init; }
+        public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
+    }
+}
