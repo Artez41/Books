05cacda7d514c84190ef1038e348a5b6e9ecd31c
Artyom <artez1997@gmail.com>
2024-09-08 17:59:51 +0300

    Add basic logging (#12)

--- src/Books.Application/Services/BookService.cs
+++ src/Books.Application/Services/BookService.cs
@@ -1,55 +1,155 @@
-﻿using Books.Application.Models;
+﻿using Books.Application.Logging;
+using Books.Application.Models;
 using Books.Application.Repositories;
+using System.Diagnostics;
 
 namespace Books.Application.Services
 {
     public class BookService : IBookService
     {
         private readonly IBookRepository _bookRepository;
+        private readonly ILoggerAdapter<BookService> _logger;
 
-        public BookService(IBookRepository bookRepository)
+        public BookService(IBookRepository bookRepository, ILoggerAdapter<BookService> logger)
         {
             _bookRepository = bookRepository;
+            _logger = logger;
         }
 
-        public Task<bool> CreateAsync(Book book, CancellationToken token = default)
+        public async Task<bool> CreateAsync(Book book, CancellationToken token = default)
         {
-            return _bookRepository.CreateAsync(book, token);
+            _logger.LogInformation("Creating book with id {0} and title: {1}", book.Id, book.Title);
+            var stopWatch = Stopwatch.StartNew();
+
+            try
+            {
+                return await _bookRepository.CreateAsync(book, token);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong while creating a book");
+                throw;
+            }
+            finally
+            {
+                stopWatch.Stop();
+                _logger.LogInformation("Book with id {0} created in {1}ms", book.Id, stopWatch.ElapsedMilliseconds); ;
+            }
         }
 
-        public Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default)
+        public async Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default)
         {
-            return _bookRepository.DeleteByIdAsync(id, token);
+            _logger.LogInformation("Deleting book with id: {0}", id);
+            var stopWatch = Stopwatch.StartNew();
+
+            try
+            {
+                return await _bookRepository.DeleteByIdAsync(id, token);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong while deleting book with id {0}", id);
+                throw;
+            }
+            finally
+            {
+                stopWatch.Stop();
+                _logger.LogInformation("Book with id {0} deleted in {1}ms", id, stopWatch.ElapsedMilliseconds); ;
+            }        
         }
 
-        public Task<IEnumerable<Book>> GetAllAsync(CancellationToken token = default)
+        public async Task<IEnumerable<Book>> GetAllAsync(CancellationToken token = default)
         {
-            return _bookRepository.GetAllAsync(token);
+            _logger.LogInformation("Retrieving all books");
+            var stopWatch = Stopwatch.StartNew();
+
+            try
+            {
+                return await _bookRepository.GetAllAsync(token);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong while retrieving all books");
+                throw;
+            }
+            finally 
+            { 
+                stopWatch.Stop();
+                _logger.LogInformation("All books retrieved in {0}ms", stopWatch.ElapsedMilliseconds);
+            }
         }
 
-        public Task<Book?> GetByIdAsync(Guid id, CancellationToken token = default)
+        public async Task<Book?> GetByIdAsync(Guid id, CancellationToken token = default)
         {
-            return _bookRepository.GetByIdAsync(id, token);
+            _logger.LogInformation("Retrieving book with id: {0}", id);
+            var stopWatch = Stopwatch.StartNew();
+
+            try
+            {
+                return await _bookRepository.GetByIdAsync(id, token);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong while retrieving book with id {0}", id);
+                throw;
+            }
+            finally
+            {
+                stopWatch.Stop();
+                _logger.LogInformation("Book with id {0} retrieved in {1}ms", id, stopWatch.ElapsedMilliseconds);
+            }
         }
 
-        public Task<Book?> GetBySlugAsync(string slug, CancellationToken token = default)
+        public async Task<Book?> GetBySlugAsync(string slug, CancellationToken token = default)
         {
-            return _bookRepository.GetBySlugAsync(slug, token);
+            _logger.LogInformation("Retrieve book by slug: {0}", slug);
+            var stopWatch = Stopwatch.StartNew();
+
+            try
+            {
+                return await _bookRepository.GetBySlugAsync(slug, token);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong while retrieving book with slug {0}", slug);
+                throw;
+            }
+            finally
+            {
+                stopWatch.Stop();
+                _logger.LogInformation("Book with slug {0} retrieved in {1}ms", slug, stopWatch.ElapsedMilliseconds);
+            }
         }
 
         public async Task<Book?> UpdateAsync(Book book, CancellationToken token = default)
         {
-            bool isBookExists = await _bookRepository.ExistsByIdAsync(book.Id, token);
+            _logger.LogInformation("Update book with id: {0}", book.Id);
+            var stopWatch = Stopwatch.StartNew();
 
-            if (!isBookExists)
-                return null;
+            try
+            {
+                bool isBookExists = await _bookRepository.ExistsByIdAsync(book.Id, token);
 
-            bool isUpdated = await _bookRepository.UpdateAsync(book, token);
+                if (!isBookExists)
+                    return null;
 
-            if (!isUpdated) 
-                return null;
+                bool isUpdated = await _bookRepository.UpdateAsync(book, token);
 
-            return book;
+                if (!isUpdated)
+                    return null;
+
+                return book;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong while updating book with id {0}", book.Id);
+                throw;
+            }
+            finally
+            {
+                stopWatch.Stop();
+                _logger.LogInformation("Book with id {0} updated in {1}ms", book.Id, stopWatch.ElapsedMilliseconds);
+            }
         }
     }
 }
--- test/Books.Tests.Unit/BookServiceTests.cs
+++ test/Books.Tests.Unit/BookServiceTests.cs
@@ -1,8 +1,11 @@
-﻿using Books.Application.Models;
+﻿using Books.Application.Logging;
+using Books.Application.Models;
 using Books.Application.Repositories;
 using Books.Application.Services;
 using FluentAssertions;
+using Microsoft.Data.Sqlite;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using NSubstitute.ReturnsExtensions;
 
 namespace Books.Application.Tests.Unit
@@ -11,10 +14,11 @@ namespace Books.Application.Tests.Unit
     {
         private readonly BookService _sut;
         private readonly IBookRepository _bookRepository = Substitute.For<IBookRepository>();
+        private readonly ILoggerAdapter<BookService> _logger = Substitute.For<ILoggerAdapter<BookService>>();
 
         public BookServiceTests()
         {
-            _sut = new BookService(_bookRepository);
+            _sut = new BookService(_bookRepository, _logger);
         }
 
         [Fact]
@@ -45,13 +49,58 @@ namespace Books.Application.Tests.Unit
         public async Task GetByIdAsync_ShouldReturnNull_WhenBookNotExists()
         {
             // Arrange
-            _bookRepository.GetByIdAsync(Arg.Any<Guid>()).ReturnsNull();
+            var bookId = Guid.NewGuid();
+            _bookRepository.GetByIdAsync(bookId).ReturnsNull();
 
             // Act
-            var result = await _sut.GetByIdAsync(Guid.NewGuid());
+            var result = await _sut.GetByIdAsync(bookId);
 
             // Assert
             result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ShouldLogMessages_WhenInvoked()
+        {
+            // Arrange
+            var book = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Lord of the rings",
+                Author = "J.R.R Tolkien",
+                Description = "Very interesting book",
+                YearOfRelease = 1923,
+                NumberOfPages = 782,
+                Genres = ["adventure"]
+            };
+
+            _bookRepository.GetByIdAsync(book.Id).Returns(book);
+
+            // Act
+            await _sut.GetByIdAsync(book.Id);
+
+            // Assert
+            _logger.Received(1).LogInformation(Arg.Is("Retrieving book with id: {0}"), book.Id);
+            _logger.Received(1).LogInformation(Arg.Is("Book with id {0} retrieved in {1}ms"), book.Id, Arg.Any<long>());
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ShouldLogMessages_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+            var sqliteException = new SqliteException("Something went wrong", 500);
+            _bookRepository.GetByIdAsync(bookId).Throws(sqliteException);
+
+            // Act
+            var requestAction = async () => await _sut.GetByIdAsync(bookId);
+
+            // Assert
+            await requestAction.Should()
+                .ThrowAsync<SqliteException>().WithMessage("Something went wrong");
+
+            _logger.Received(1).LogInformation(Arg.Is("Retrieving book with id: {0}"), bookId);
+            _logger.Received(1).LogError(sqliteException, Arg.Is("Something went wrong while retrieving book with id {0}"), bookId);
         }
 
         [Fact]
@@ -92,7 +141,61 @@ namespace Books.Application.Tests.Unit
         }
 
         [Fact]
-        public async Task CreateAsync_ShouldCreateBook_WhenBookCreated()
+        public async Task GetBySlugAsync_ShouldLogMessages_WhenInvoked()
+        {
+            // Arrange
+            var book = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Lord of the rings",
+                Author = "J.R.R Tolkien",
+                Description = "Very interesting book",
+                YearOfRelease = 1923,
+                NumberOfPages = 782,
+                Genres = ["adventure"]
+            };
+
+            _bookRepository.GetBySlugAsync(book.Slug).Returns(book);
+
+            // Act
+            await _sut.GetBySlugAsync(book.Slug);
+
+            // Assert
+            _logger.Received(1).LogInformation(Arg.Is("Retrieve book by slug: {0}"), Arg.Any<string>());
+            _logger.Received(1).LogInformation(Arg.Is("Book with slug {0} retrieved in {1}ms"), Arg.Any<string>(), 
+                Arg.Any<long>());
+        }
+
+        [Fact]
+        public async Task GetBySlugAsync_ShouldLogMessages_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var book = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Lord of the rings",
+                Author = "J.R.R Tolkien",
+                Description = "Very interesting book",
+                YearOfRelease = 1923,
+                NumberOfPages = 782,
+                Genres = ["adventure"]
+            };
+
+            var sqliteException = new SqliteException("Something went wrong", 500);
+            _bookRepository.GetBySlugAsync(book.Slug).Throws(sqliteException);
+
+            // Act
+            var requestAction = async () => await _sut.GetBySlugAsync(book.Slug);
+
+            // Assert
+            await requestAction.Should()
+                .ThrowAsync<SqliteException>().WithMessage("Something went wrong");
+            _logger.Received(1).LogInformation(Arg.Is("Retrieve book by slug: {0}"), book.Slug);
+            _logger.Received(1).LogError(sqliteException, Arg.Is("Something went wrong while retrieving book with slug {0}"), book.Slug);
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldReturnTrue_WhenBookCreated()
         {
             // Arrange
             var book = new Book
@@ -113,6 +216,84 @@ namespace Books.Application.Tests.Unit
 
             // Assert
             result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldReturnFalse_WhenBookNotCreated()
+        {
+            // Arrange
+            var book = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Lord of the rings",
+                Author = "J.R.R Tolkien",
+                Description = "Very interesting book",
+                YearOfRelease = 1923,
+                NumberOfPages = 782,
+                Genres = ["adventure"]
+            };
+
+            _bookRepository.CreateAsync(book).Returns(false);
+
+            // Act
+            var result = await _sut.CreateAsync(book);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldLogMessages_WhenInvoked()
+        {
+            // Arrange
+            var book = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Lord of the rings",
+                Author = "J.R.R Tolkien",
+                Description = "Very interesting book",
+                YearOfRelease = 1923,
+                NumberOfPages = 782,
+                Genres = ["adventure"]
+            };
+
+            _bookRepository.CreateAsync(book).Returns(true);
+
+            // Act
+            await _sut.CreateAsync(book);
+
+            // Assert
+            _logger.Received(1).LogInformation(Arg.Is("Creating book with id {0} and title: {1}"), book.Id, book.Title);
+            _logger.Received(1).LogInformation(Arg.Is("Book with id {0} created in {1}ms"), book.Id, Arg.Any<long>());
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldLogMessages_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var book = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Lord of the rings",
+                Author = "J.R.R Tolkien",
+                Description = "Very interesting book",
+                YearOfRelease = 1923,
+                NumberOfPages = 782,
+                Genres = ["adventure"]
+            };
+
+            var sqliteException = new SqliteException("Something went wrong", 500);
+            _bookRepository.CreateAsync(book).ThrowsAsync(sqliteException);
+
+            // Act
+            var requestAction = async () => await _sut.CreateAsync(book);
+
+            // Assert
+            await requestAction.Should()
+                .ThrowAsync<SqliteException>().WithMessage("Something went wrong");
+
+            _logger.Received(1).LogInformation(Arg.Is("Creating book with id {0} and title: {1}"), book.Id, book.Title);
+            _logger.Received(1).LogError(sqliteException, Arg.Is("Something went wrong while creating a book"));
         }
 
         [Fact]
@@ -156,6 +337,36 @@ namespace Books.Application.Tests.Unit
 
             // Assert
             result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldLogMessages_WhenInvoked()
+        {
+            // Arrange
+            _bookRepository.GetAllAsync().Returns(Enumerable.Empty<Book>());
+
+            // Act
+            await _sut.GetAllAsync();
+
+            // Assert
+            _logger.Received(1).LogInformation(Arg.Is("Retrieving all books"));
+            _logger.Received(1).LogInformation(Arg.Is("All books retrieved in {0}ms"), Arg.Any<long>());
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldLogMessageAndException_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var sqliteException = new SqliteException("Something went wrong", 500);
+            _bookRepository.GetAllAsync().Throws(sqliteException);
+
+            // Act
+            var requestAction = async () => await _sut.GetAllAsync();
+
+            // Assert
+            await requestAction.Should()
+                .ThrowAsync<SqliteException>().WithMessage("Something went wrong");
+            _logger.Received(1).LogError(Arg.Is(sqliteException), Arg.Is("Something went wrong while retrieving all books"));
         }
 
         [Fact]
@@ -234,6 +445,62 @@ namespace Books.Application.Tests.Unit
         }
 
         [Fact]
+        public async Task UpdateAsync_ShouldLogMessages_WhenInvoked()
+        {
+            // Arrange
+            var updatedBook = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Crime and Punishment",
+                Author = "Fyodor Dostoevsky",
+                Description = "Phylosofy book",
+                YearOfRelease = 1866,
+                NumberOfPages = 527,
+                Genres = ["adventure"]
+            };
+
+            _bookRepository.ExistsByIdAsync(updatedBook.Id).Returns(true);
+            _bookRepository.UpdateAsync(updatedBook).Returns(true);
+
+            // Act
+            var result = await _sut.UpdateAsync(updatedBook);
+
+            // Assert
+            _logger.Received(1).LogInformation(Arg.Is("Update book with id: {0}"), updatedBook.Id);
+            _logger.Received(1).LogInformation(Arg.Is("Book with id {0} updated in {1}ms"), updatedBook.Id, Arg.Any<long>());
+        }
+
+        [Fact]
+        public async Task UpdateAsync_ShouldLogMessages_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var updatedBook = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Crime and Punishment",
+                Author = "Fyodor Dostoevsky",
+                Description = "Phylosofy book",
+                YearOfRelease = 1866,
+                NumberOfPages = 527,
+                Genres = ["adventure"]
+            };
+
+            var sqliteException = new SqliteException("Something went wrong", 500);
+            _bookRepository.ExistsByIdAsync(updatedBook.Id).Returns(true);
+            _bookRepository.UpdateAsync(updatedBook).ThrowsAsync(sqliteException);
+
+            // Act
+            var requestAction = async () => await _sut.UpdateAsync(updatedBook);
+
+            // Assert
+            await requestAction.Should()
+                .ThrowAsync<SqliteException>().WithMessage("Something went wrong");
+
+            _logger.Received(1).LogInformation(Arg.Is("Update book with id: {0}"), updatedBook.Id);
+            _logger.Received(1).LogError(sqliteException, Arg.Is("Something went wrong while updating book with id {0}"), updatedBook.Id);
+        }
+
+        [Fact]
         public async Task DeleteByIdAsync_ShouldDeleteBook_WhenBookExists()
         {
             // Arrange
@@ -259,6 +526,40 @@ namespace Books.Application.Tests.Unit
 
             // Assert
             result.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task DeleteByIdAsync_ShouldLogMessages_WhenInvoked()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+            _bookRepository.DeleteByIdAsync(bookId).Returns(true);
+
+            // Act
+            var result = await _sut.DeleteByIdAsync(bookId);
+
+            // Assert
+            _logger.LogInformation(Arg.Is("Deleting book with id: {0}"), bookId);
+            _logger.LogInformation(Arg.Is("Book with id {0} deleted in {1}ms"), bookId, Arg.Any<long>());
+        }
+
+        [Fact]
+        public async Task DeleteByIdAsync_ShouldLogMessages_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+            var sqliteException = new SqliteException("Something went wrong", 500);
+            _bookRepository.DeleteByIdAsync(bookId).ThrowsAsync(sqliteException);
+
+            // Act
+            var requestAction = async () => await _sut.DeleteByIdAsync(bookId);
+
+            // Assert
+            await requestAction.Should()
+                .ThrowAsync<SqliteException>().WithMessage("Something went wrong");
+
+            _logger.LogInformation(Arg.Is("Deleting book with id: {0}"), bookId);
+            _logger.LogError(sqliteException, Arg.Is("Book with id {0} deleted in {1}ms"), bookId, Arg.Any<long>());
         }
     }
 }
