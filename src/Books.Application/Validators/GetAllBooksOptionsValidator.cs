07582c83228636218485da1ede2c9deb6ccf0358
Artyom Bobkov <artez1997@gmail.com>
2024-09-10 22:09:37 +0300

    Add validator for GetAllBooksOptions

---
+++ src/Books.Application/Validators/GetAllBooksOptionsValidator.cs
@@ -0,0 +1,23 @@
+﻿using Books.Application.Models;
+using FluentValidation;
+
+namespace Books.Application.Validators
+{
+    public class GetAllBooksOptionsValidator : AbstractValidator<GetAllBooksOptions>
+    {
+        public static readonly string[] AcceptableSortFields =
+        {
+            "yearofrelease"
+        };
+
+        public GetAllBooksOptionsValidator()
+        {
+            RuleFor(x => x.Page)
+                .GreaterThanOrEqualTo(1);
+
+            RuleFor(x => x.PageSize)
+                .InclusiveBetween(1, 25)
+                .WithMessage("You can get between 1 and 25 books per page");
+        }
+    }
+}
