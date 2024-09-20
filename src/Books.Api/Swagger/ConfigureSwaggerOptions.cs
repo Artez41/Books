77ae9c564327320a9f000239248c310e6caa6f13
Artyom Bobkov <artez1997@gmail.com>
2024-09-20 23:34:52 +0300

    Integrate versioning in Swagger

---
+++ src/Books.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,34 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Books.Api.Swagger
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+        private readonly IHostEnvironment _environment;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider, IHostEnvironment hostEnvironment)
+        {
+            _environment = hostEnvironment;
+            _provider = apiVersionDescriptionProvider;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(
+                    description.GroupName,
+                    new OpenApiInfo
+                    {
+                        Title = _environment.ApplicationName,
+                        Version = description.ApiVersion.ToString()
+                    });
+            }
+        }
+    }
+}
