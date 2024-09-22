0e02459495d8d880ee809d5d24d865efdfaf7ced
Artyom Bobkov <artez1997@gmail.com>
2024-09-22 18:26:30 +0300

    Implement api key-based auth

--- src/Books.Api/Program.cs
+++ src/Books.Api/Program.cs
@@ -46,6 +46,8 @@ builder.Services.AddAuthorization(x =>
     );
 });
 
+builder.Services.AddScoped<ApiKeyAuthFilter>();
+
 builder.Services.AddApiVersioning(x =>
 {
     x.DefaultApiVersion = new ApiVersion(1.0);
